7aa5edf0c8e6f06051e1fa2d2e712b0d91cc7839
Thanh Dao <thanh.dao50@thehutgroup.com>
2022-10-28 00:53:46 +0100

    First commit

---
+++ src/test/java/com/deliverysystem/dispatchsystem/DispatchsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deliverysystem.dispatchsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DispatchsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
