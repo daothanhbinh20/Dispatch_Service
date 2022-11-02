8121c8f23eb8205363d9b2325f6503ecf80cc137
Thanh Dao <thanh.dao50@thehutgroup.com>
2022-11-02 20:23:15 +0000

    Add Controller layer for CreateOrder Endpoint

--- src/main/java/com/deliverysystem/dispatchsystem/facade/OrderResource.java
+++ src/main/java/com/deliverysystem/dispatchsystem/facade/OrderResource.java
@@ -1,18 +1,37 @@
 package com.deliverysystem.dispatchsystem.facade;
 
 import com.deliverysystem.dispatchsystem.entities.Order;
+import com.deliverysystem.dispatchsystem.repository.exception.RestaurantNotFoundException;
+import com.deliverysystem.dispatchsystem.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping(path = "order")
 public class OrderResource {
 
+  private final OrderService orderService;
+
+  @Autowired
+  public OrderResource(final OrderService orderService) {
+    this.orderService = orderService;
+  }
+
+
   @PostMapping("/")
-  public ResponseEntity<String> createOrder(@RequestBody final Order order) {
-    return null;
+  public ResponseEntity<String> createOrder(@RequestBody Order order) {
+    int orderId = order.getOrderId();
+    int restaurantId = order.getRestaurantId();
+    try {
+      orderService.createOrder(order);
+      return ResponseEntity.status(HttpStatus.CREATED).body("Created an Order with id: " + orderId);
+    } catch (RestaurantNotFoundException ex) {
+      return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Unable to find restaurant of ID: " + restaurantId);
+    }
   }
 }
